2a1d07e59ebb8bfdba707c03cd0994eebf5906e1
jaychang99 <jaychang99@gmail.com>
2023-02-19 12:42:42 +0900

    feat: TodoItem 에 GET: /todos API 연결

--- src/components/common/Todo/Todo.tsx
+++ src/components/common/Todo/Todo.tsx
@@ -1,12 +1,26 @@
 import { Checkbox } from '@mui/material';
 import { Box } from '@mui/system';
+import { clientAxios } from 'api/commonAxios';
+import { fetcher } from 'api/fetcher';
 import TodoItem from 'components/common/Todo/_elements/TodoItem/TodoItem';
 import React from 'react';
+import { useQuery } from 'react-query';
+import { TodoApiResponse } from 'types/api/todo';
 
 function Todo() {
+  const query = useQuery<TodoApiResponse>('todos', () => {
+    return fetcher('/todos');
+  });
+
   return (
     <div>
-      <TodoItem>마트에서 우유 사기</TodoItem>
+      {query?.data?.result.map((todo) => (
+        <>
+          <TodoItem key={todo.id} {...todo}>
+            {todo.name}
+          </TodoItem>
+        </>
+      ))}
     </div>
   );
 }
--- src/components/common/Todo/_elements/TodoItem/TodoItem.tsx
+++ src/components/common/Todo/_elements/TodoItem/TodoItem.tsx
@@ -3,14 +3,15 @@ import React from 'react';
 
 interface Props {
   children: string;
+  isCompleted: boolean;
 }
 
-function TodoItem({ children }: Props) {
+function TodoItem({ children, isCompleted }: Props) {
   return (
     <div>
       <FormGroup>
         <FormControlLabel
-          control={<Checkbox defaultChecked />}
+          control={<Checkbox checked={isCompleted} />}
           label={children}
         />
       </FormGroup>
