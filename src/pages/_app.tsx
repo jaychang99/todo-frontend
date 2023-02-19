80b9a38999f282ccafeb73b3766c64fe9aa36922
jaychang99 <jaychang99@gmail.com>
2023-02-19 12:41:37 +0900

    feat: react-query 인스턴스 생성 및 QueryClientProvider 을 _app.tsx 에 생성

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,20 @@
 import { CssBaseline, ThemeProvider } from '@mui/material';
 import { Container } from '@mui/system';
 import type { AppProps } from 'next/app';
+import { QueryClientProvider } from 'react-query';
 import { globalStyles } from 'styles/globalStyles';
+import { queryClient } from 'api/reactQuery';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={globalStyles}>
-      <CssBaseline />
-      <Container>
-        <Component {...pageProps} />
-      </Container>
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={globalStyles}>
+        <CssBaseline />
+        <Container>
+          <Component {...pageProps} />
+        </Container>
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 }
 
